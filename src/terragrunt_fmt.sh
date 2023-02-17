4d35512f8b1c2bd63ce0583ba515cdbe00d85604
mherreral <mherlop77@gmail.com>
2023-02-17 09:36:04 -0500

    fixed more vars to be snakecase

--- src/terragrunt_fmt.sh
+++ src/terragrunt_fmt.sh
@@ -2,38 +2,38 @@
 
 function terragrunt_fmt {
 	# Eliminate `-recursive` option for Terragrunt 0.11.x.
-	fmtRecursive="-recursive"
+	fmt_recursive="-recursive"
 	if hasPrefix "0.11" "${tf_version}"; then
-		fmtRecursive=""
+		fmt_recursive=""
 	fi
 
 	# Gather the output of `terragrunt fmt`.
 	echo "fmt: info: checking if Terragrunt files in ${tf_working_dir} are correctly formatted"
-	fmtOutput=$(${tf_binary} fmt -check=true -write=false -diff ${fmtRecursive} ${*} 2>&1)
-	fmtExitCode=${?}
+	fmt_output=$(${tf_binary} fmt -check=true -write=false -diff ${fmt_recursive} ${*} 2>&1)
+	fmt_exit_code=${?}
 
 	# Exit code of 0 indicates success. Print the output and exit.
-	if [ ${fmtExitCode} -eq 0 ]; then
+	if [ ${fmt_exit_code} -eq 0 ]; then
 		echo "fmt: info: Terragrunt files in ${tf_working_dir} are correctly formatted"
-		echo "${fmtOutput}"
+		echo "${fmt_output}"
 		echo
-		exit ${fmtExitCode}
+		exit ${fmt_exit_code}
 	fi
 
 	# Exit code of 2 indicates a parse error. Print the output and exit.
-	if [ ${fmtExitCode} -eq 2 ]; then
+	if [ ${fmt_exit_code} -eq 2 ]; then
 		echo "fmt: error: failed to parse Terragrunt files"
-		echo "${fmtOutput}"
+		echo "${fmt_output}"
 		echo
-		exit ${fmtExitCode}
+		exit ${fmt_exit_code}
 	fi
 
 	# Exit code of !0 and !2 indicates failure.
 	echo "fmt: error: Terragrunt files in ${tf_working_dir} are incorrectly formatted"
-	echo "${fmtOutput}"
+	echo "${fmt_output}"
 	echo
 	echo "fmt: error: the following files in ${tf_working_dir} are incorrectly formatted"
-	fmtFileList=$(${tf_binary} fmt -check=true -write=false -list ${fmtRecursive})
+	fmtFileList=$(${tf_binary} fmt -check=true -write=false -list ${fmt_recursive})
 	echo "${fmtFileList}"
 	echo
 
@@ -53,27 +53,27 @@ ${fmtFileDiff}
 
 		done
 
-		fmtCommentWrapper="#### \`${tf_binary} fmt\` Failed
+		fmt_comment_wrapper="#### \`${tf_binary} fmt\` Failed
 ${fmtComment}
 
 *Workflow: \`${GITHUB_WORKFLOW}\`, Action: \`${GITHUB_ACTION}\`, Working Directory: \`${tf_working_dir}\`, Workspace: \`${tf_workspace}\`*"
 
-		fmtCommentWrapper=$(stripColors "${fmtCommentWrapper}")
+		fmt_comment_wrapper=$(stripColors "${fmt_comment_wrapper}")
 		echo "fmt: info: creating JSON"
-		fmtPayload=$(echo "${fmtCommentWrapper}" | jq -R --slurp '{body: .}')
-		fmtCommentsURL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
+		fmt_payload=$(echo "${fmt_comment_wrapper}" | jq -R --slurp '{body: .}')
+		fmt_comments_url=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
 		echo "fmt: info: commenting on the pull request"
-		echo "${fmtPayload}" | curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "${fmtCommentsURL}" >/dev/null
+		echo "${fmt_payload}" | curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "${fmt_comments_url}" >/dev/null
 	fi
 
 	# Write changes to branch
 	echo "::set-output name=tf_actions_fmt_written::false"
 	if [ "${tf_fmt_write}" == "1" ]; then
 		echo "fmt: info: Terraform files in ${tf_working_dir} will be formatted"
-		terraform fmt -write=true ${fmtRecursive} "${*}"
-		fmtExitCode=${?}
+		terraform fmt -write=true ${fmt_recursive} "${*}"
+		fmt_exit_code=${?}
 		echo "::set-output name=tf_actions_fmt_written::true"
 	fi
 
-	exit ${fmtExitCode}
+	exit ${fmt_exit_code}
 }
--- src/terragrunt_hclfmt.sh
+++ src/terragrunt_hclfmt.sh
@@ -8,41 +8,41 @@ function terragrunt_hcl_fmt {
 		exit 0
 	fi
 
-	fmtOutput=$(${tf_binary} hclfmt --terragrunt-check)
-	fmtExitCode=${?}
+	fmt_output=$(${tf_binary} hclfmt --terragrunt-check)
+	fmt_exit_code=${?}
 
 	# Exit code of 0 indicates success. Print the output and exit.
-	if [ ${fmtExitCode} -eq 0 ]; then
+	if [ ${fmt_exit_code} -eq 0 ]; then
 		echo "hclfmt: info: Terragrunt files in ${tf_working_dir} are correctly formatted"
-		echo "${fmtOutput}"
+		echo "${fmt_output}"
 		echo
-		exit ${fmtExitCode}
+		exit ${fmt_exit_code}
 	else
 		echo "hclfmt: error: failed to format Terragrunt files"
-		echo "${fmtOutput}"
+		echo "${fmt_output}"
 	fi
 
 	if [ "$GITHUB_EVENT_NAME" == "pull_request" ] && [ "${tf_comment}" == "1" ]; then
-		fmtCommentWrapper="#### \`${tf_binary} hclfmt\` Failed:
-# echo "FMT OUTPUT ${fmtOutput}"
+		fmt_comment_wrapper="#### \`${tf_binary} hclfmt\` Failed:
+# echo "FMT OUTPUT ${fmt_output}"
 *Workflow: \`${GITHUB_WORKFLOW}\`, Action: \`${GITHUB_ACTION}\`, Working Directory: \`${tf_working_dir}\`, Workspace: \`${tf_workspace}\`*"
 
-		fmtCommentWrapper=$(stripColors "${fmtCommentWrapper}")
+		fmt_comment_wrapper=$(stripColors "${fmt_comment_wrapper}")
 		echo "fmt: info: creating JSON"
-		fmtPayload=$(echo "${fmtCommentWrapper}" | jq -R --slurp '{body: .}')
-		fmtCommentsURL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
+		fmt_payload=$(echo "${fmt_comment_wrapper}" | jq -R --slurp '{body: .}')
+		fmt_comments_url=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
 		echo "fmt: info: commenting on the pull request"
-		echo "${fmtPayload}" | curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "${fmtCommentsURL}" >/dev/null
+		echo "${fmt_payload}" | curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "${fmt_comments_url}" >/dev/null
 	fi
 
 	# Write changes to branch
 	echo "tf_actions_fmt_written=false" >>$GITHUB_OUTPUT
 	if [ "${tf_fmt_write}" == "1" ]; then
 		echo "fmt: info: Terraform files in ${tf_working_dir} will be formatted"
-		terraform fmt -write=true ${fmtRecursive} "${*}"
-		fmtExitCode=${?}
+		terraform fmt -write=true ${fmt_recursive} "${*}"
+		fmt_exit_code=${?}
 		echo "tf_actions_fmt_written=true" >>$GITHUB_OUTPUT
 	fi
 
-	exit ${fmtExitCode}
+	exit ${fmt_exit_code}
 }
